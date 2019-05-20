01de2092fb051ebcb4dff8f2662abdab63f2619b
Alexander Feoktistov <alexander.feoktistov@corp.mail.ru>
2019-05-20 19:25:07 +0300

    Game for mobius 2019 - part one.

---
+++ mobius2019/precache-manifest.12794f6bd83dcc3e62d95cf7acbfe9f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c45a77467b7389b532cb52828c89c126",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5e649b5ca517dd145a4",
+    "url": "/static/css/main.42a0b11c.chunk.css"
+  },
+  {
+    "revision": "9020278cc894011e92fd",
+    "url": "/static/js/2.7b960028.chunk.js"
+  },
+  {
+    "revision": "a5e649b5ca517dd145a4",
+    "url": "/static/js/main.cc047527.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "94e7dd4218fa8a09cfabc9fbc9d4a2b9",
+    "url": "/static/media/oklogo.94e7dd42.svg"
+  }
+]);
