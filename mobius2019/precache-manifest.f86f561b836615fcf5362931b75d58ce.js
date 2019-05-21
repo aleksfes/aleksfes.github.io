0efce8403a7c3f540b06c5dab09571eeca4b8698
Alexander Feoktistov <alexander.feoktistov@corp.mail.ru>
2019-05-21 16:52:14 +0300

    new build

---
+++ mobius2019/precache-manifest.f86f561b836615fcf5362931b75d58ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "005a303acd0e6991026d01c4082858fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b516f5863db46f4cbe79",
+    "url": "static/css/main.bf5af515.chunk.css"
+  },
+  {
+    "revision": "4fcd545a6a53ece80696",
+    "url": "static/js/2.0a1a94f0.chunk.js"
+  },
+  {
+    "revision": "b516f5863db46f4cbe79",
+    "url": "static/js/main.6ddaa53a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "94e7dd4218fa8a09cfabc9fbc9d4a2b9",
+    "url": "static/media/oklogo.94e7dd42.svg"
+  }
+]);
--- mobius2019/service-worker.js
+++ mobius2019/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.db3bbc77d55ced5442a1e41ebcb5e4cb.js"
+  "/precache-manifest.f86f561b836615fcf5362931b75d58ce.js"
 );
 
 self.addEventListener('message', (event) => {
