6fa11491e5946f68628fd45e64d4d42aa7ee91a6
Alexander Feoktistov <alexander.feoktistov@corp.mail.ru>
2019-05-21 16:18:25 +0300

    pathes fixed

--- mobius2019/precache-manifest.db3bbc77d55ced5442a1e41ebcb5e4cb.js
+++ mobius2019/precache-manifest.db3bbc77d55ced5442a1e41ebcb5e4cb.js
@@ -5,22 +5,22 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "690547d78046e22892b6",
-    "url": "/static/css/main.bf5af515.chunk.css"
+    "url": "static/css/main.bf5af515.chunk.css"
   },
   {
     "revision": "daf8beb7a6f7f0e3a877",
-    "url": "/static/js/2.d0c347a8.chunk.js"
+    "url": "static/js/2.d0c347a8.chunk.js"
   },
   {
     "revision": "690547d78046e22892b6",
-    "url": "/static/js/main.7203a29c.chunk.js"
+    "url": "static/js/main.7203a29c.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
-    "url": "/static/js/runtime~main.a8a9905a.js"
+    "url": "static/js/runtime~main.a8a9905a.js"
   },
   {
     "revision": "94e7dd4218fa8a09cfabc9fbc9d4a2b9",
-    "url": "/static/media/oklogo.94e7dd42.svg"
+    "url": "static/media/oklogo.94e7dd42.svg"
   }
 ]);
