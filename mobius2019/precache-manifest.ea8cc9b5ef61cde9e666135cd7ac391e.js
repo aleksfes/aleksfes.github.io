53d645ad6ee78242da5b3ee3d76a82b084260542
Alexander Feoktistov <alexander.feoktistov@corp.mail.ru>
2019-05-22 12:21:27 +0300

    Categories will be logged to the console, 'Start' button became bigger.

---
+++ mobius2019/precache-manifest.ea8cc9b5ef61cde9e666135cd7ac391e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ce66a9351f2ee730b5fd69b10993fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9f63d5aeab1acdb1634",
+    "url": "static/css/main.2962905b.chunk.css"
+  },
+  {
+    "revision": "4fcd545a6a53ece80696",
+    "url": "static/js/2.0a1a94f0.chunk.js"
+  },
+  {
+    "revision": "c9f63d5aeab1acdb1634",
+    "url": "static/js/main.ea31cb12.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "94e7dd4218fa8a09cfabc9fbc9d4a2b9",
+    "url": "static/media/oklogo.94e7dd42.svg"
+  }
+]);
--- mobius2019/service-worker.js
+++ mobius2019/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.62f991a70e64be7c9503ca4c3f8ff4b0.js"
+  "/precache-manifest.ea8cc9b5ef61cde9e666135cd7ac391e.js"
 );
 
 self.addEventListener('message', (event) => {
